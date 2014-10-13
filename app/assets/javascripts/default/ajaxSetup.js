df5f3a2de9cb3ea00a6fd86cab6d822fc9e8f6e7
Vivek <vivekg342@gmail.com>
2014-10-13 17:51:42 +0800

    ajax setup

---
+++ app/assets/javascripts/default/ajaxSetup.js
@@ -0,0 +1,22 @@
+// $.xhrPool and $.ajaxSetup
+// Store all the pending requests
+
+$.xhrPool = [];
+$.xhrPool.abortAll = function() {
+    $(this).each(function(idx, jqXHR) {
+        jqXHR.abort();
+    });
+    $.xhrPool = [];
+};
+
+$.ajaxSetup({
+    beforeSend: function(jqXHR) {
+        $.xhrPool.push(jqXHR);
+    },
+    complete: function(jqXHR) {
+        var index = $.xhrPool.indexOf(jqXHR);
+        if (index > -1) {
+            $.xhrPool.splice(index, 1);
+        }
+    }
+});
