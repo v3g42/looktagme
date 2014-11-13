6583aba713a75beb26c04e23656706c42d7d9686
Vivek <vivekg342@gmail.com>
2014-11-13 17:03:38 +0800

    change log level

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,5 @@ TagConda::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.logger = Logger.new(STDOUT)
 end
