b8b601463107645b887c63d73540b2dc5861ef0c
Vivek <vivekg342@gmail.com>
2014-11-13 17:02:19 +0800

    change log level

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,6 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
-TagConda::Application.initialize!
+TagConda::Application.initialize
+
+ActiveRecord::Base.logger.level = Logger::DEBUG
