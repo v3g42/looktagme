741f4f4fc49e840f5e85a3a51adc480a03a55531
Vivek <vivekg342@gmail.com>
2014-12-15 17:09:35 +0800

    embed_looktag_me

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   respond_to :html, :json
   caches_action :index, :about, :howitworks, :layout => false
+  before_action :authenticate_user!, :only => :embed
   def index
     @page = params[:page] || 1
     @limit = params[:limit] || 30
